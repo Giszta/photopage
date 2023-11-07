cb431d50736bd3c06a49e5024b5eff5ab7259285
AdamGiszter <a.m.giszter@gmail.com>
2023-11-07 14:24:42 +0100

    about me section, photobox improvement, cleanup my code

---
+++ js/navigation.js
@@ -0,0 +1,62 @@
+//---------NAVBAR--------------
+const nav = document.querySelector("#page-navigation");
+
+nav.innerHTML = `<nav class="navbar">
+<div class="nav-center">
+    <div class="nav-header">
+        <img src="/photopage/img/logo.webp" class="logo" alt="logo" />
+    </div>
+    <!--links-->
+    <ul class="links">
+        <li><a href="index.html">Strona Główna</a></li>
+        <li><a href="photoGallery.html">Galeria Zdjęć</a></li>
+        <li><a href="aboutMe.html">O mnie</a></li>
+    </ul>
+</div>
+</nav>
+<img src="/photopage/img/logo.webp" class="side-logo" alt="logo" />
+<button class="sidebar-toggle">
+<span class="bar"></span>
+<span class="bar"></span>
+<span class="bar"></span>
+</button>
+<aside class="sidebar">
+<!-- <div class="sidebar-header">
+    <img src="/photopage/img/logo.webp" class="logo" alt="logo" />
+</div> -->
+<!--links-->
+<ul class="links">
+    <li><a href="index.html">Strona Główna</a></li>
+    <li><a href="photoGallery.html">Galeria Zdjęć</a></li>
+    <li><a href="aboutMe.html">O mnie</a></li>
+</ul>
+<!--social media-->
+<ul class="social-icons">
+    <li>
+        <a href="https://www.instagram.com/romangiszter/" target="_blank"
+            ><i class="fa fa-instagram"></i
+        ></a>
+    </li>
+    <li>
+        <a href="https://www.facebook.com/profile.php?id=100007263227928" target="_blank"
+            ><i class="fa fa-facebook"></i
+        ></a>
+    </li>
+    <li>
+        <a href="mailto: kontakt@fotoroman.pl" target="_blank"
+            ><i class="fa fa-envelope"></i
+        ></a>
+    </li>
+</ul>
+<p class="aside-copyright"></p>
+</aside>`;
+
+const navToggle = document.querySelector(".sidebar-toggle");
+const links = document.querySelector(".sidebar");
+const bodyOverflow = document.querySelector("body");
+
+navToggle.addEventListener("click", () => {
+	links.classList.toggle("show-sidebar");
+	bodyOverflow.classList.toggle("stopScroll");
+	navToggle.classList.toggle("active");
+});
---
+++ js/photoGallery.js
@@ -0,0 +1,664 @@
+//---------ALBUM PHOTOS--------------
+const allAlbumPhotos = {
+	graffiti: [
+		{
+			id: 1,
+			class: "thumbnail first",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/001.jpg",
+		},
+		{
+			id: 2,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/002.jpg",
+		},
+		{
+			id: 3,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/003.jpg",
+		},
+		{
+			id: 4,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/004.jpg",
+		},
+		{
+			id: 5,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/005.jpg",
+		},
+		{
+			id: 6,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/006.jpg",
+		},
+		{
+			id: 7,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/007.jpg",
+		},
+		{
+			id: 8,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/008.jpg",
+		},
+		{
+			id: 9,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/009.jpg",
+		},
+		{
+			id: 10,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/010.jpg",
+		},
+		{
+			id: 11,
+			class: "thumbnail",
+			title: "Graffity",
+			filter: "inne",
+			src: "./img/gallery/graffiti/011.jpg",
+		},
+	],
+	hluboka: [
+		{
+			id: 1,
+			class: "thumbnail first",
+			title: "Zamek Hluboká",
+			filter: "Europa",
+			src: "./img/gallery/Zamek_Hluboká/001.jpg",
+		},
+		{
+			id: 2,
+			class: "thumbnail",
+			title: "Zamek Hluboká",
+			filter: "Europa",
+			src: "./img/gallery/Zamek_Hluboká/002.jpg",
+		},
+		{
+			id: 3,
+			class: "thumbnail",
+			title: "Zamek Hluboká",
+			filter: "Europa",
+			src: "./img/gallery/Zamek_Hluboká/003.jpg",
+		},
+		{
+			id: 4,
+			class: "thumbnail",
+			title: "Zamek Hluboká",
+			filter: "Europa",
+			src: "./img/gallery/Zamek_Hluboká/004.jpg",
+		},
+		{
+			id: 5,
+			class: "thumbnail",
+			title: "Zamek Hluboká",
+			filter: "Europa",
+			src: "./img/gallery/Zamek_Hluboká/005.jpg",
+		},
+		{
+			id: 6,
+			class: "thumbnail",
+			title: "Zamek Hluboká",
+			filter: "Europa",
+			src: "./img/gallery/Zamek_Hluboká/006.jpg",
+		},
+		{
+			id: 7,
+			class: "thumbnail",
+			title: "Zamek Hluboká",
+			filter: "Europa",
+			src: "./img/gallery/Zamek_Hluboká/007.jpg",
+		},
+		{
+			id: 8,
+			class: "thumbnail",
+			title: "Zamek Hluboká",
+			filter: "Europa",
+			src: "./img/gallery/Zamek_Hluboká/008.jpg",
+		},
+	],
+	sopot23: [
+		{
+			id: 1,
+			class: "thumbnail first",
+			title: "Sopot",
+			filter: "Polska",
+			src: "./img/gallery/Sopot2023/001.jpg",
+		},
+		{
+			id: 2,
+			class: "thumbnail",
+			title: "Sopot",
+			filter: "Polska",
+			src: "./img/gallery/Sopot2023/002.jpg",
+		},
+		{
+			id: 3,
+			class: "thumbnail",
+			title: "Sopot",
+			filter: "Polska",
+			src: "./img/gallery/Sopot2023/003.jpg",
+		},
+		{
+			id: 4,
+			class: "thumbnail",
+			title: "Sopot",
+			filter: "Polska",
+			src: "./img/gallery/Sopot2023/004.jpg",
+		},
+		{
+			id: 5,
+			class: "thumbnail",
+			title: "Sopot",
+			filter: "Polska",
+			src: "./img/gallery/Sopot2023/005.jpg",
+		},
+		{
+			id: 6,
+			class: "thumbnail",
+			title: "Sopot",
+			filter: "Polska",
+			src: "./img/gallery/Sopot2023/006.jpg",
+		},
+		{
+			id: 7,
+			class: "thumbnail",
+			title: "Sopot",
+			filter: "Polska",
+			src: "./img/gallery/Sopot2023/007.jpg",
+		},
+		{
+			id: 8,
+			class: "thumbnail",
+			title: "Sopot",
+			filter: "Polska",
+			src: "./img/gallery/Sopot2023/008.jpg",
+		},
+	],
+	slowacja22: [
+		{
+			id: 1,
+			class: "thumbnail first",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/001.jpg",
+		},
+		{
+			id: 2,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/002.jpg",
+		},
+		{
+			id: 3,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/003.jpg",
+		},
+		{
+			id: 4,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/004.jpg",
+		},
+		{
+			id: 5,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/005.jpg",
+		},
+		{
+			id: 6,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/006.jpg",
+		},
+		{
+			id: 7,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/007.jpg",
+		},
+		{
+			id: 8,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/008.jpg",
+		},
+		{
+			id: 9,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/009.jpg",
+		},
+		{
+			id: 10,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/010.jpg",
+		},
+		{
+			id: 11,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/011.jpg",
+		},
+		{
+			id: 12,
+			class: "thumbnail",
+			title: "Słowacja 2022",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2022/012.jpg",
+		},
+	],
+	slowacja23: [
+		{
+			id: 1,
+			class: "thumbnail first",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/001.jpg",
+		},
+		{
+			id: 2,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/002.jpg",
+		},
+		{
+			id: 3,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/003.jpg",
+		},
+		{
+			id: 4,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/004.jpg",
+		},
+		{
+			id: 5,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/005.jpg",
+		},
+		{
+			id: 6,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/006.jpg",
+		},
+		{
+			id: 7,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/007.jpg",
+		},
+		{
+			id: 8,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/008.jpg",
+		},
+		{
+			id: 9,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/009.jpg",
+		},
+		{
+			id: 10,
+			class: "thumbnail",
+			title: "Słowacja 2023",
+			filter: "Polska",
+			src: "./img/gallery/Slowacja2023/010.jpg",
+		},
+	],
+};
+
+//---------VARIABLES--------------
+
+const mainPhoto = document.querySelector(".main-photo");
+const activeMainPhoto = document.querySelector("#main-photo");
+const photoCounter = document.querySelector(".photo-counter");
+const arrowLeft = document.querySelector("#prev-btn");
+const arrowRight = document.querySelector("#next-btn");
+const filterBtns = document.querySelectorAll(".filter-btn");
+const activeOnload = document.querySelector("#active-btn");
+const albumCover = document.querySelectorAll(".photo");
+const albumCoverTitle = document.querySelectorAll(".album-title");
+const photoBoxToggle = document.querySelectorAll(".photo");
+const photoBox = document.querySelector(".photobox");
+const closeBtn = document.querySelector(".close-btn-container");
+const autoplayBtn = document.querySelector(".autoplay-btn");
+const playBtn = document.querySelector(".fa-play");
+const pauseBtn = document.querySelector(".fa-pause");
+const thumbsToggler = document.querySelector(".thumbs-toggler");
+const chevronRotate = document.querySelector(".fa-chevron-down");
+const thumbsHide = document.querySelector(".caption");
+const rotateBtn = document.querySelector(".rotate-btn");
+const thumbnailsContainer = document.querySelector(".thumbnails");
+const thumbnails = document.querySelectorAll(".thumbnail");
+const firstActiveItem = document.querySelector(".first");
+const albums = document.querySelectorAll(".section-center .gallery-item	");
+const timer = document.querySelector(".circle-progress-box");
+const d = (el) => {
+	if (!el.classList.contains("hidden")) {
+		el.classList.add("hidden");
+	}
+	setTimeout(() => el.classList.toggle("hidden"), 0);
+};
+const progressBarDown = [
+	{ transform: "rotate(0)" },
+	{ transform: "rotate(90deg)" },
+];
+const progressBarUp = [
+	{ transform: "rotate(90deg)" },
+	{ transform: "rotate(0)" },
+];
+let firstThumbnail;
+let clickedAlbumId;
+let allThumbnails;
+let clickedThumbnail;
+let activeBtn;
+let d1;
+let d2;
+let timeout;
+let interval;
+let imgInterval;
+
+//---------PHOTO GALLERY--------------
+
+filterBtns.forEach((e) => {
+	e.addEventListener("click", onButtonClick);
+});
+
+albumCover.forEach((item) => {
+	albumName = item.id;
+	item.src = allAlbumPhotos[albumName][0].src;
+	item.addEventListener("click", getClickedAlbumId);
+});
+
+albumCoverTitle.forEach((item) => {
+	albumTitle = item.id;
+	item.innerHTML = allAlbumPhotos[albumTitle][0].title;
+});
+
+photoBoxToggle.forEach((albumCover) => {
+	albumCover.addEventListener("click", () => {
+		activeAlbum = albumCover.id;
+		photoBox.classList.toggle("active");
+		bodyOverflow.classList.toggle("stop-scroll");
+		activeMainPhoto.src = allAlbumPhotos[activeAlbum][0].src;
+		activeMainPhoto.dataset.index = firstThumbnail.dataset.index;
+		firstThumbnail.classList.add("active-thumbnail");
+		photoCounter.innerHTML = `(${Number(activeMainPhoto.dataset.index) + 1}/${
+			allAlbumPhotos[clickedAlbumId].length
+		})`;
+		window.scrollTo(0, 0);
+	});
+});
+
+autoplayBtn.addEventListener("click", () => {
+	playBtn.classList.toggle("hidden");
+	pauseBtn.classList.toggle("hidden");
+	function autoplayImg() {
+		if (playBtn.classList.contains("hidden")) {
+			imgInterval = setInterval(showNextImg, 5000);
+			firstAnimation = firstprogressBarAnimation();
+			timeout = setTimeout(() => {
+				interval = setInterval(() => {
+					d1 = timer.animate(progressBarDown, {
+						duration: 4700,
+					});
+					d2 = timer.animate(progressBarUp, {
+						delay: 4700,
+						duration: 300,
+					});
+				}, 5000);
+			}, 300);
+		} else {
+			stopProgressBarAnimation();
+		}
+	}
+	autoplayImg();
+});
+
+closeBtn.addEventListener("click", close);
+
+arrowRight.addEventListener("click", showNextImg);
+
+arrowLeft.addEventListener("click", showPrevImg);
+
+thumbsToggler.addEventListener("click", () => {
+	chevronRotate.classList.toggle("chevron-rotate");
+	thumbsHide.classList.toggle("thumbs-hide");
+	mainPhoto.classList.toggle("main-photo-max");
+	chevronRotate.classList.add("transition");
+	thumbsHide.classList.add("transition");
+	mainPhoto.classList.add("transition");
+	setTimeout(() => {
+		thumbsHide.classList.remove("transition");
+		chevronRotate.classList.remove("transition");
+		mainPhoto.classList.remove("transition");
+	}, 500);
+});
+
+document.addEventListener("keydown", (e) => {
+	if (photoBox.classList.contains("active")) {
+		if (e.code === "ArrowRight" || e.keyCode === 39) {
+			showNextImg();
+		} else if (e.code === "ArrowLeft" || e.keyCode === 37) {
+			showPrevImg();
+		} else if (e.code === "Escape" || e.keyCode === 27) {
+			close();
+		}
+	}
+});
+
+//---------FUNCTIONS--------------
+
+function onButtonClick(e) {
+	if (activeBtn) {
+		if (activeBtn != e.currentTarget) {
+			activeBtn.classList.remove("active-btn");
+		}
+	}
+	activeOnload.classList.remove("active-btn");
+	activeBtn = e.currentTarget;
+	activeBtn.classList.add("active-btn");
+}
+
+function filterAlbums(e) {
+	const albums = document.querySelectorAll(".section-center .gallery-item	");
+	const filter = e.target.dataset.filter;
+
+	if (filter === "*") {
+		return albums.forEach(d);
+	}
+
+	return albums.forEach((album) => {
+		album.classList.contains(filter) ? d(album) : album.classList.add("hidden");
+	});
+}
+
+function getClickedAlbumId(e) {
+	clickedAlbumId = e.target.id;
+	displayThumbsitems(allAlbumPhotos[clickedAlbumId]);
+}
+
+function close() {
+	bodyOverflow.classList.toggle("stop-scroll");
+	thumbnails.forEach((item) => item.classList.remove("active-thumbnail"));
+	photoBox.classList.add("fade-out");
+	setTimeout(() => {
+		photoBox.classList.toggle("active");
+		photoBox.classList.remove("fade-out");
+	}, 500);
+
+	if (playBtn.classList.contains("hidden")) {
+		playBtn.classList.toggle("hidden");
+		pauseBtn.classList.toggle("hidden");
+		stopProgressBarAnimation();
+	}
+}
+
+function displayThumbsitems(thumbnails) {
+	let displayThumbnails = thumbnails.map(function (item, index) {
+		return `<li><img data-index=${index} class="${item.class}" src="${item.src}" alt="${item.title}"></li>`;
+	});
+	displayThumbnails = displayThumbnails.join("");
+	thumbnailsContainer.innerHTML = displayThumbnails;
+	firstThumbnail = document.querySelector("li img[data-index='0']");
+	allThumbnails = document.querySelectorAll(".thumbnail");
+
+	allThumbnails.forEach((e) => {
+		e.addEventListener("click", activeItem);
+	});
+}
+
+function activeItem(e) {
+	clickedThumbnail = e.target.dataset.index;
+	activeMainPhoto.dataset.index = clickedThumbnail;
+	const thumbnails = document.querySelector(
+		`.thumbnail[data-index='${clickedThumbnail}']`
+	);
+	allThumbnails.forEach((item) => {
+		if (item.dataset.index !== activeMainPhoto.dataset.index)
+			item.classList.remove("active-thumbnail");
+	});
+	photoCounter.innerHTML = `(${Number(activeMainPhoto.dataset.index) + 1}/${
+		allAlbumPhotos[clickedAlbumId].length
+	})`;
+	mainPhoto.classList.add("fade-out");
+	setTimeout(() => {
+		activeMainPhoto.src = allAlbumPhotos[clickedAlbumId][clickedThumbnail].src;
+	}, 500);
+	setTimeout(() => {
+		mainPhoto.classList.remove("fade-out");
+	}, 1000);
+	thumbnails.classList.add("active-thumbnail");
+}
+
+function firstprogressBarAnimation() {
+	const firstProgressBarDown = timer.animate(progressBarDown, {
+		duration: 5000,
+	});
+	const firstProgressBarUp = timer.animate(progressBarUp, {
+		delay: 5000,
+		duration: 300,
+	});
+	return [firstProgressBarDown, firstProgressBarUp];
+}
+
+function stopProgressBarAnimation() {
+	clearInterval(interval);
+	clearTimeout(timeout);
+	clearInterval(imgInterval);
+	d1?.cancel();
+	d2?.cancel();
+	firstAnimation.forEach((progressBarAnimation) => {
+		progressBarAnimation.cancel();
+	});
+}
+
+function showNextImg() {
+	let currentIndexValue = Number(activeMainPhoto.getAttribute("data-index"));
+	if (currentIndexValue === allAlbumPhotos[clickedAlbumId].length - 1) {
+		nextIndexValue = 0;
+	} else {
+		nextIndexValue = currentIndexValue + 1;
+	}
+	currentIndexValue = nextIndexValue;
+	activeMainPhoto.dataset.index = currentIndexValue;
+	let activeThumbnail = document.querySelector(
+		`.thumbnail[data-index='${currentIndexValue}']`
+	);
+	const thumbnails = document.querySelectorAll(".thumbnail");
+	thumbnails.forEach((item) => {
+		if (item.dataset.index !== activeMainPhoto.dataset.index)
+			item.classList.remove("active-thumbnail");
+	});
+	photoCounter.innerHTML = `(${Number(activeMainPhoto.dataset.index) + 1}/${
+		allAlbumPhotos[clickedAlbumId].length
+	})`;
+	mainPhoto.classList.add("fade-out");
+	setTimeout(() => {
+		activeMainPhoto.src = allAlbumPhotos[clickedAlbumId][currentIndexValue].src;
+	}, 500);
+	setTimeout(() => {
+		mainPhoto.classList.remove("fade-out");
+	}, 1000);
+	activeThumbnail.classList.add("active-thumbnail");
+}
+
+function showPrevImg() {
+	let currentIndexValue = Number(activeMainPhoto.getAttribute("data-index"));
+	if (currentIndexValue === 0) {
+		previousIndexValue = allAlbumPhotos[clickedAlbumId].length - 1;
+	} else {
+		previousIndexValue = --currentIndexValue;
+	}
+	currentIndexValue = previousIndexValue;
+	activeMainPhoto.dataset.index = currentIndexValue;
+	// activeMainPhoto.src = albumPhotos[CURRENT_INDEX_VALUE].img;
+	let activeThumbnail = document.querySelector(
+		`.thumbnail[data-index='${currentIndexValue}']`
+	);
+	const thumbnails = document.querySelectorAll(".thumbnail");
+	thumbnails.forEach((item) => {
+		if (item.dataset.index !== activeMainPhoto.dataset.index)
+			item.classList.remove("active-thumbnail");
+	});
+	photoCounter.innerHTML = `(${Number(activeMainPhoto.dataset.index) + 1}/${
+		allAlbumPhotos[clickedAlbumId].length
+	})`;
+	mainPhoto.classList.add("fade-out");
+
+	setTimeout(() => {
+		activeMainPhoto.src = allAlbumPhotos[clickedAlbumId][currentIndexValue].src;
+	}, 500);
+	setTimeout(() => {
+		mainPhoto.classList.remove("fade-out");
+	}, 1000);
+	activeThumbnail.classList.add("active-thumbnail");
+}
